aa7a6b2bb85196aaccf2a2c1a0dd05ff78e654cd
Revina3105 <revina.utami31@gmail.com>
2021-02-23 16:43:08 +0700

    add tugas 9

---
+++ src/consts/categories.js
@@ -0,0 +1,8 @@
+const categories = [
+  {id: '1', name: 'Rok', image: require('../assets/catergories/rok.jpg')},
+  {id: '2', name: 'Jilbab', image: require('../assets/catergories/jilbab.jpg')},
+  {id: '3', name: 'Sepatu', image: require('../assets/catergories/sepatu.jpg')},
+  {id: '4', name: 'Baju', image: require('../assets/catergories/baju.jpg')},
+];
+
+export default categories;
---
+++ src/consts/produks.js
@@ -0,0 +1,32 @@
+const produks = [
+  {
+    id: '1',
+    name: 'Jilbab',
+    ingredients: 'Katun',
+    price: '15.000',
+    image: require('../assets/jilbab.jpg'),
+  },
+  {
+    id: '2',
+    name: 'Rok',
+    ingredients: 'Katun',
+    price: '70.000',
+    image: require('../assets/rok.jpg'),
+  },
+  {
+    id: '3',
+    name: 'Sepatu',
+    ingredients: 'Karet',
+    price: '45.000',
+    image: require('../assets/sepatu.jpg'),
+  },
+  {
+    id: '4',
+    name: 'Baju',
+    ingredients: 'Sutra',
+    price: '100.000',
+    image: require('../assets/baju.jpg'),
+  },
+];
+
+export default produks;
